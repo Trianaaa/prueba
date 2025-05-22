a303b97f960093eb807451239ea68836ac75705c
Pablo Triana <triana@Mac-mini-de-Pablo.local>
2025-05-22 12:17:52 -0500

    more fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import { Github, Sun, Moon } from "lucide-react";
-import { useTheme } from 'next-themes';
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -24,16 +24,35 @@ export default function Home() {
         >
           <Github className="w-5 h-5 text-black dark:text-white" />
         </a>
+
+        {/* Botón Dark Mode */}
+        {mounted && (
+          <button
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            aria-label="Toggle Dark Mode"
+            className="p-2 bg-gray-200 dark:bg-gray-700 rounded-full hover:bg-gray-300 dark:hover:bg-gray-600 transition"
+          >
+            {theme === "dark" ? (
+              <Sun className="w-5 h-5 text-yellow-500" />
+            ) : (
+              <Moon className="w-5 h-5 text-blue-800" />
+            )}
+          </button>
+        )}
       </div>
 
       {/* Contenido principal */}
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="text-center sm:text-left max-w-xl">
-          <h1 className="text-3xl font-bold mb-4">Prueba Técnica - Gestión de Usuarios</h1>
+          <h1 className="text-3xl font-bold mb-4">
+            Prueba Técnica - Gestión de Usuarios
+          </h1>
           <p className="text-base text-gray-700 dark:text-gray-300">
-            Esta aplicación permite consultar, agregar y eliminar usuarios usando una API externa.
-            Se muestran únicamente los usuarios activos y se brinda una interfaz amigable para gestionar
-            sus datos. Desarrollada con NextJS + React, TailwindCSS y desplegada en Vercel para facilitar su evaluación.
+            Esta aplicación permite consultar, agregar y eliminar usuarios
+            usando una API externa. Se muestran únicamente los usuarios activos
+            y se brinda una interfaz amigable para gestionar sus datos.
+            Desarrollada con NextJS + React, TailwindCSS y desplegada en Vercel
+            para facilitar su evaluación.
           </p>
         </div>
 
